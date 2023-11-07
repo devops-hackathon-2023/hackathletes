43bc9211ae58e5093edefdb39cacc41ebb30473e
cratas <petr8kratochvil@seznam.cz>
2023-11-07 19:33:12 +0100

    feature: login
    added dashboard route for default logged view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,31 +15,3 @@ const Home = () => (
 );
 
 export default Home;
-
-// import AllSases from '@/components/sections/AllSases';
-// import { AppBar } from '@/components/main-page/AppBar';
-// import RecentSases from '@/components/sections/RecentSases';
-// import { Stack } from '@mui/material';
-// import { atomRecentSases } from '@/constants/state/atoms';
-// import { useAtom } from 'jotai';
-
-// const Home = () => {
-//   const [, setRecentSasess] = useAtom(atomRecentSases);
-
-//   const clearRecentSases = () => setRecentSasess([]);
-
-//   return (
-//     <>
-//       <AppBar />
-//       <button type="button" onClick={clearRecentSases}>
-//         clear recent sases
-//       </button>
-//       <Stack paddingX={15} spacing={2} paddingTop={2}>
-//         <RecentSases />
-//         <AllSases />
-//       </Stack>
-//     </>
-//   );
-// };
-
-// export default Home;
