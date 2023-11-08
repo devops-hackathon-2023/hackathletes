d0490ae41412ee3a3e4978d0e39fc9b709e0d3aa
Lukas Bajer <bajkylb@gmail.com>
2023-11-08 08:33:30 +0100

    update: added user into login menu and set up automatic login

--- src/components/LoginOptionsCard.tsx
+++ src/components/LoginOptionsCard.tsx
@@ -1,53 +1,73 @@
-import { Link as MUILink, Box, Button, Card, CardContent, CardHeader, Stack, Typography } from '@mui/material';
-import { useLocales } from '@/locales';
+import {Link as MUILink, Box, Button, Card, CardContent, CardHeader, Stack, Typography} from '@mui/material';
+import {useLocales} from '@/locales';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
-import { useRouter } from 'next/router';
+import {useRouter} from 'next/router';
+import {useAtom} from "jotai";
+import {loggedUserAtom} from "@/state/atoms";
+import {useFetchUser} from "@/queries";
+import {DEFAULT_LOGIN_USER_ID} from "@/constants";
+import {useEffect} from "react";
 
 export const LoginOptionsCard = () => {
-  const router = useRouter();
+    const [user, setUser] = useAtom(loggedUserAtom);
+    const fetchData = useFetchUser(DEFAULT_LOGIN_USER_ID);
+    const router = useRouter();
 
-  const { t, onChangeLang, currentLang } = useLocales();
-
-  const handleChangeLang = () => onChangeLang(currentLang.value === 'en' ? 'cs' : 'en');
-
-  return (
-    <Card maxWidth="sm" component={Stack} m="auto" sx={{ mt: 3 }}>
-      <CardHeader
-        title={t('logIn')}
-        titleTypographyProps={{ variant: 'h4' }}
-        subheader={t('logInDopo')}
-        action={
-          <Button variant="text" size="large" fullWidth onClick={handleChangeLang}>
-            {currentLang.value === 'en' ? 'CZ' : 'EN'}
-          </Button>
+    useEffect(() => {
+        if (fetchData.data) {
+            setUser(fetchData.data);
         }
-      />
+    }, [fetchData.data, setUser]);
 
-      <CardContent component={Stack} spacing={2}>
-        <Button
-          variant="outlined"
-          size="large"
-          fullWidth
-          startIcon={<AlternateEmailIcon />}
-          onClick={() => router.push('auth/login')}
-        >
-          {t('logWithMail')}
-        </Button>
+    useEffect(() => {
+        if (user) {
+            router.push('/dashboard')
+        }
+    }, [router, user])
 
-        <Button disabled variant="contained" size="large" fullWidth startIcon={<GitHubIcon />}>
-          {t('logWithGitHub')}
-        </Button>
+    const {t, onChangeLang, currentLang} = useLocales();
 
-        <Box sx={{ pt: 2 }}>
-          <Typography variant="body1" sx={{ display: 'inline' }}>
-            {t('noAccYet')}{' '}
-          </Typography>
-          <MUILink href="/auth/register" variant="body1" sx={{ display: 'inline', textDecoration: 'none', fontWeight: 'bold' }}>
-            {t('signUp')}
-          </MUILink>
-        </Box>
-      </CardContent>
-    </Card>
-  );
+    const handleChangeLang = () => onChangeLang(currentLang.value === 'en' ? 'cs' : 'en');
+
+    return (
+        <Card maxWidth="sm" component={Stack} m="auto" sx={{mt: 3}}>
+            <CardHeader
+                title={t('logIn')}
+                titleTypographyProps={{variant: 'h4'}}
+                subheader={t('logInDopo')}
+                action={
+                    <Button variant="text" size="large" fullWidth onClick={handleChangeLang}>
+                        {currentLang.value === 'en' ? 'CZ' : 'EN'}
+                    </Button>
+                }
+            />
+
+            <CardContent component={Stack} spacing={2}>
+                <Button
+                    variant="outlined"
+                    size="large"
+                    fullWidth
+                    startIcon={<AlternateEmailIcon/>}
+                    onClick={() => router.push('auth/login')}
+                >
+                    {t('logWithMail')}
+                </Button>
+
+                <Button disabled variant="contained" size="large" fullWidth startIcon={<GitHubIcon/>}>
+                    {t('logWithGitHub')}
+                </Button>
+
+                <Box sx={{pt: 2}}>
+                    <Typography variant="body1" sx={{display: 'inline'}}>
+                        {t('noAccYet')}{' '}
+                    </Typography>
+                    <MUILink href="/auth/register" variant="body1"
+                             sx={{display: 'inline', textDecoration: 'none', fontWeight: 'bold'}}>
+                        {t('signUp')}
+                    </MUILink>
+                </Box>
+            </CardContent>
+        </Card>
+    );
 };
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,28 +1,14 @@
 import AllSases from '@/components/sections/AllSases';
 import Favourites from '@/components/sections/Favourites';
-import { useAtom } from 'jotai';
-import { useFetchUser } from '@/queries';
-import { useEffect } from 'react';
-import { DEFAULT_LOGIN_USER_ID } from '@/constants';
-import {loggedUserAtom} from "@/state/atoms";
 import MainPageLayout from "@/components/main-page/MainPageLayout";
 
-const Home = () => {
-    const [user, setUser] = useAtom(loggedUserAtom);
-    const fetchData = useFetchUser(DEFAULT_LOGIN_USER_ID);
+alert('Automatically logged in') // TODO: login logic in LoginOptionsCard
 
-    useEffect(() => {
-        if (fetchData.data) {
-            setUser(fetchData.data);
-        }
-    }, [fetchData.data, setUser]);
-
-    return (
-        <MainPageLayout>
-            <Favourites />
-            <AllSases />
-        </MainPageLayout>
-    );
-};
+const Home = () => (
+    <MainPageLayout>
+        <Favourites/>
+        <AllSases/>
+    </MainPageLayout>
+);
 
 export default Home;
