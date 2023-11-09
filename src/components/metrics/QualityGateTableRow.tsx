ce0498bc9d63ba4d57cf5ad2256cf37510ccedcb
Dominik Vít <slipiklp@gmail.com>
2023-11-09 18:45:11 +0100

    Feat  new metrics (#53)
    * feat: new metrics page
    
    * feat: new metrics

--- src/components/metrics/QualityGateTableRow.tsx
+++ src/components/metrics/QualityGateTableRow.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { styled } from '@mui/material/styles';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import { formatQualityType } from '@/utils/qualityGate';
 
 const SuccessIcon = styled(CheckCircleOutlineIcon)(({ theme }) => ({
   color: theme.palette.success.main,
@@ -14,26 +15,7 @@ const ErrorIcon = styled(ErrorOutlineIcon)(({ theme }) => ({
 }));
 
 const resolveResultIcon = (result: string) => (result === 'PASSED' ? <SuccessIcon /> : <ErrorIcon />);
-const formatQualityType = (qualityType: string): string => {
-  switch (qualityType) {
-    case 'SECURITY_HOTSPOTS_REVIEWED':
-      return 'Security hotspots reviewed';
-    case 'CODE_COVERAGE':
-      return 'Code coverage';
-    case 'DUPLICATED_LINES':
-      return 'Duplicated lines';
-    case 'XRAY_DOCKER':
-      return 'Xray Docker';
-    case 'MAINTAINABILITY_RATING':
-      return 'Maintainability rating';
-    case 'RELIABILITY_RATING':
-      return 'Reliability rating';
-    case 'SECURITY_RATING':
-      return 'Security rating';
-    default:
-      throw new Error(`Unknown QualityType: ${qualityType}`);
-  }
-};
+
 const QualityGateTableRow = ({ gate }: { gate: QualityGate }) => {
   const { type, result, rating, percent } = gate;
 
@@ -45,7 +27,7 @@ const QualityGateTableRow = ({ gate }: { gate: QualityGate }) => {
         {percent ? (
           <Stack direction="row" alignItems="center" spacing={2}>
             <LinearProgress sx={{ width: '150px' }} variant="determinate" value={percent} />
-            <Typography>{percent} %</Typography>
+            <Typography whiteSpace="nowrap">{percent} %</Typography>
           </Stack>
         ) : (
           rating
