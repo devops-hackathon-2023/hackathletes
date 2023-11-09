1301b209daf06e47b00b8075b4800e25ae9a41f8
cratas <petr8kratochvil@seznam.cz>
2023-11-09 18:19:43 +0100

    feature: custom avatar update

--- src/components/CustomAvatar.tsx
+++ src/components/CustomAvatar.tsx
@@ -1,4 +1,15 @@
-import { Avatar, IconButton, ListItemButton, Menu, MenuItem, Skeleton, Stack, Typography } from '@mui/material';
+import {
+  Avatar,
+  Box,
+  Divider,
+  IconButton,
+  ListItemButton,
+  MenuItem,
+  Popover,
+  Skeleton,
+  Stack,
+  Typography,
+} from '@mui/material';
 
 import UnfoldMoreIcon from '@mui/icons-material/UnfoldMore';
 import { useGetUser } from '@/queries';
@@ -6,6 +17,7 @@ import { useAtom } from 'jotai';
 import { loggedUserAtom } from '@/state/atoms';
 import { useState } from 'react';
 import { useRouter } from 'next/router';
+import { useLocales } from '@/locales';
 
 export const CustomAvatar = () => {
   const [loggedUser, setLoggedUser] = useAtom(loggedUserAtom);
@@ -13,6 +25,7 @@ export const CustomAvatar = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const { data: user, isLoading } = useGetUser(loggedUser?.id ?? '');
+  const { t } = useLocales();
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
@@ -22,26 +35,38 @@ export const CustomAvatar = () => {
   };
 
   const handleLogout = () => {
+    router.push('/');
     sessionStorage.removeItem('user');
     setLoggedUser(null);
-    router.push('/');
   };
 
   if (isLoading) return <Skeleton animation="wave" width={248} height={58} />;
 
   return (
     <>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
+      <Popover
         open={open}
+        anchorEl={anchorEl}
         onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'center' }}
       >
-        <MenuItem onClick={handleLogout}>Logout</MenuItem>
-      </Menu>
+        <Box sx={{ p: 2, pb: 1.5 }}>
+          <Typography variant="subtitle2" noWrap>
+            {user?.name}
+          </Typography>
+
+          <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
+            {user?.email}
+          </Typography>
+        </Box>
+
+        <Divider />
+
+        <MenuItem onClick={handleLogout} sx={{ fontWeight: 'fontWeightBold', color: 'error.main' }}>
+          {t('logout')}
+        </MenuItem>
+      </Popover>
 
       <ListItemButton
         sx={{
